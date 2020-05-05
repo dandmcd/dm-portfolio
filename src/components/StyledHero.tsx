1f373fabfdc2d7e4a4a47c9ab83ca8fd09a3ce1f
givionte <dandmcd@live.com>
2020-05-05 11:31:28 +0800

    Gatsby incremental builds

--- src/components/StyledHero.tsx
+++ src/components/StyledHero.tsx
@@ -8,7 +8,7 @@ const StyledHero = ({ className }) => {
     <StaticQuery
       query={graphql`
         query {
-          defaultbg: file(relativePath: { eq: "hero/splash.jpg" }) {
+          defaultbg: file(relativePath: { eq: "hero/computerguy.jpg" }) {
             childImageSharp {
               fluid(quality: 90, maxWidth: 1920) {
                 ...GatsbyImageSharpFluid
@@ -37,13 +37,14 @@ export default styled(StyledHero)`
   width: 100vw;
   height: 100vh;
   display: block;
+  background: radial-gradient(
+    ellipse at center,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(0, 0, 0, 0) 20%,
+    rgba(0, 0, 0, 0.65) 100%
+  );
   position: fixed;
-  background-color: #fff8f4;
   background-size: cover;
   background-position: center center;
-  filter: contrast(80%) blur(4px);
-  /* background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1200 800'%3E%3Cdefs%3E%3CradialGradient id='a' cx='0' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23fef6c4'/%3E%3Cstop offset='1' stop-color='%23fef6c4' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='b' cx='1200' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23fee8c6'/%3E%3Cstop offset='1' stop-color='%23fee8c6' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='c' cx='600' cy='0' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23f0fc97'/%3E%3Cstop offset='1' stop-color='%23f0fc97' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='d' cx='600' cy='800' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23fff8f4'/%3E%3Cstop offset='1' stop-color='%23fff8f4' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='e' cx='0' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23d6fa96'/%3E%3Cstop offset='1' stop-color='%23d6fa96' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='f' cx='1200' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23fdef98'/%3E%3Cstop offset='1' stop-color='%23fdef98' stop-opacity='0'/%3E%3C/radialGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='1200' height='800'/%3E%3Crect fill='url(%23b)' width='1200' height='800'/%3E%3Crect fill='url(%23c)' width='1200' height='800'/%3E%3Crect fill='url(%23d)' width='1200' height='800'/%3E%3Crect fill='url(%23e)' width='1200' height='800'/%3E%3Crect fill='url(%23f)' width='1200' height='800'/%3E%3C/svg%3E");
-   background-attachment: fixed;
-   background-size: cover;
-   */
+  filter: contrast(85%) brightness(80%);
 `
--- src/css/globalstyled.tsx
+++ src/css/globalstyled.tsx
@@ -26,10 +26,6 @@ export const theme = {
 export const GlobalStyle = createGlobalStyle`
   body {
     z-index: 1;
-    background-color: #000;
-background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1200 800'%3E%3Cdefs%3E%3CradialGradient id='a' cx='0' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23fff48d'/%3E%3Cstop offset='1' stop-color='%23fff48d' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='b' cx='1200' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23ffed52'/%3E%3Cstop offset='1' stop-color='%23ffed52' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='c' cx='600' cy='0' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23ffe43b'/%3E%3Cstop offset='1' stop-color='%23ffe43b' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='d' cx='600' cy='800' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23fffaa4'/%3E%3Cstop offset='1' stop-color='%23fffaa4' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='e' cx='0' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23ffed76'/%3E%3Cstop offset='1' stop-color='%23ffed76' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='f' cx='1200' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23ffd900'/%3E%3Cstop offset='1' stop-color='%23ffd900' stop-opacity='0'/%3E%3C/radialGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='1200' height='800'/%3E%3Crect fill='url(%23b)' width='1200' height='800'/%3E%3Crect fill='url(%23c)' width='1200' height='800'/%3E%3Crect fill='url(%23d)' width='1200' height='800'/%3E%3Crect fill='url(%23e)' width='1200' height='800'/%3E%3Crect fill='url(%23f)' width='1200' height='800'/%3E%3C/svg%3E");
-background-attachment: fixed;
-background-size: cover;
   font-family: "Barlow Semi Condensed", sans-serif;
   margin: 0 auto;
   padding: 0;
